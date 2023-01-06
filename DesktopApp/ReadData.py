0366c9f9148c6181b90d79e8b58e7039c0381098
Felipe <cataofelipe@gmail.com>
2023-01-06 01:49:32 -0300

    update

--- DesktopApp/ReadData.py
+++ DesktopApp/ReadData.py
@@ -4,16 +4,22 @@ import xml.etree.ElementTree as ET
 class ReadDataBase:
     def __init__(self) -> None:
         pass
-    def ReturnData():
-        file = minidom.parse("./MyBox/UserBase.xml")
-        tagGet = file.getElementsByTagName('Database')
-        return [tagGet[0].firstChild.data,tagGet[0].attributes['name'].value]
+    ElementsNode = ["titulo","subtitulo","tasks"]
+    BodyElement = {}
+    TasksElements = []
+    def ReturnData(self):
+        tree = ET.parse('./MyBox/UserBase.xml')
+        root = tree.getroot()
+        for Elem in self.ElementsNode:
+            for child in root.findall("./nootbook[@name='inicial']/"+Elem):
+                if Elem == "tasks":
+                    self.TasksElements.append(child.text)
+                else:
+                    self.BodyElement[Elem] = child.text
+        print(self.TasksElements)
+        return self.BodyElement
         
-        
+    def GetTasks(self):
+        return self.TasksElements  
     def ReadData():
-        file = "./MyBox/UserBase.xml"
-        tagGet = ET.parse(file)
-        rootElement = tagGet.getroot()
-        for element in rootElement.findall("NomeBase"):
-           element.find("Database").text = "<h3>TESTE</h3>"
-        tagGet.write(file,encoding='UTF-8',xml_declaration=True)
+        return 0
--- DesktopApp/app.py
+++ DesktopApp/app.py
@@ -7,6 +7,8 @@ from ConfigBox import ConfigOpt
 from ReadData import ReadDataBase
 from Configurates import Config
 import RenderWidgets
+from PIL import Image
+from PIL import ImageTk
 
 # Configurações padrões da Pagina
 master = Tk()
@@ -22,11 +24,14 @@ conf.ReadThemesInSystem()
 #Estilos customizados para a pagina 
 StylePageMenu = Style()
 StylePageMenu.configure("A.TFrame",background = conf.ColorClass[0]["background"])
+StylePageMenu.configure("B.TFrame",background=conf.ColorClass[0]["color"],foreground=conf.ColorClass[0]["color"])
 
 #Importação de informação
 
 
-t = ReadDataBase.ReturnData()
+t = ReadDataBase()
+t = t.ReturnData()
+print(t)
 r = RenderWidgets.RenderWidget()
 
 ReadDataBase.ReadData()
@@ -44,19 +49,34 @@ Bt_newCol.bind("<Button>" ,lambda e: NewData(master))
 tese = "abc"
 master.bind('r', lambda event: r.UpdateItem(l1,tese,master))
 
+#Adicionar tasks
+TaskCreated =  ReadDataBase().GetTasks()
+Tasks =""
+for value in TaskCreated:
+    Tasks += "<h4 style='color:"+conf.ColorClass[0]["color"]+";'>"+value+"</h4>"
+    print(Tasks)
+var1 = ""
+
 l1 = HTMLLabel(master, html=""" 
 <div style='background-color:"""+conf.ColorClass[0]["background"]+"""; font-family: 'Chivo Mono', monospace;'>
-<h1 style='color:"""+conf.ColorClass[0]["color"]+""";'background-color:#6c1e7b; font-family: 'Open Sans', sans-serif;'>"""+t[1]+"""</h1>
-<h2 style='color:"""+conf.ColorClass[0]["color"]+""";font-family: 'Chivo Mono', monospace;'>"""+t[0]+tese+"""</h2>
+<h1 style='color:"""+conf.ColorClass[0]["color"]+""";'background-color:#6c1e7b; font-family: 'Open Sans', sans-serif;'>"""+t["titulo"]+"""</h1>
+<h2 style='color:"""+conf.ColorClass[0]["color"]+""";font-family: 'Chivo Mono', monospace;'>"""+t["subtitulo"]+"""</h2>
+"""+Tasks+"""
 </div>
     """,font=("Calibri", 10))
+photo = Image.open("./img/Check.png")
+photo = photo.resize((40,40),Image.ANTIALIAS)
+photoImg =  ImageTk.PhotoImage(photo)
+# Teste para criação de Tasks Isoladas
+c1 = Button(l1, text='Python',image=photoImg,compound = LEFT)
+
 menu_frame.pack(fill="x",pady=4)
 Bt_newCol.pack(side="left")
 Bt_updateCol.pack(side="left")
 Bt_Config.pack(side="left")
 l1.configure(bg=conf.ColorClass[0]["background"])
 l1.pack(fill="both", expand=True,padx=400,pady=50)
-
+c1.pack(side="left")
 #Fim do conteudo da Pagina
 
 
