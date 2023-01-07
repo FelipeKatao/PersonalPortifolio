150af41760f6f0589e6918906ecb3b0cc87628bd
Felipe <cataofelipe@gmail.com>
2023-01-07 15:06:32 -0300

    update docs

--- PyData/app.py
+++ PyData/app.py
@@ -8,10 +8,13 @@ Cargos = pd.read_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\Cargos.csv")
 #sqlQUery = dsql.DataPySql.ReadFristQuery("D:\Pessoal\CataoChanel\Tutos\PyData\sql\View_usuarios.sql")
 #print(ps.sqldf(sqlQUery,locals()))
 
-# Localizar os Usuarios apenas (Localizar por nome / Por todos)
+# Localizar os Usuarios apenas
 usuarios = pd.read_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\usuarios.csv")
 df = pd.DataFrame(usuarios)
+#df.index=[df.Usario]
 df.index=[0,0,0,0,0,0]
-df.loc[0,"Acessos"] = 0
+# ou  print(df.loc["Lucas"])
+df.loc[0,"Acessos"] = 1
 print(df)
 df.to_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\usuarios.csv",index=False)
+
