7afab2a9eca63e59c8afa4feb5a14f88b98141e7
Felipe <cataofelipe@gmail.com>
2023-01-07 15:02:35 -0300

    set value add

--- PyData/app.py
+++ PyData/app.py
@@ -8,9 +8,10 @@ Cargos = pd.read_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\Cargos.csv")
 #sqlQUery = dsql.DataPySql.ReadFristQuery("D:\Pessoal\CataoChanel\Tutos\PyData\sql\View_usuarios.sql")
 #print(ps.sqldf(sqlQUery,locals()))
 
-SqlQuery = dsql.DataPySql()
-Qr = SqlQuery.ExecAllQuery("PyData\sql\View_usuarios.sql")
-for line in Qr:
-    print(ps.sqldf(line,locals()))
-
-
+# Localizar os Usuarios apenas (Localizar por nome / Por todos)
+usuarios = pd.read_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\usuarios.csv")
+df = pd.DataFrame(usuarios)
+df.index=[0,0,0,0,0,0]
+df.loc[0,"Acessos"] = 0
+print(df)
+df.to_csv(R"D:\Pessoal\CataoChanel\Tutos\PyData\DataBase\usuarios.csv",index=False)
